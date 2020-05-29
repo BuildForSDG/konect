010f532037fa309dd590f5ed7cb37ea56c14655d
Aminu Muhammed <bulangu97@gmail.com>
2020-05-29 11:17:37 +0100

    started working on product page

--- src/product.php
+++ src/product.php
@@ -1,3 +1,20 @@
 <?php
+include_once('./gen/function.php');
+ 
+session_start();
 
+//return to login if not logged in
+if (!isset($_SESSION['user']) ||(trim ($_SESSION['user']) == '')){
+	header('Location: ./home.php');
+}
+$user = new model();
+
+//fetch user data
+$sql = "SELECT * FROM users WHERE id = '".$_SESSION['user']."'";
+$row = $user->details($sql);
+
+require_once('header.php');
 ?>
+
+
+<?php require_once('footer.php')?>
