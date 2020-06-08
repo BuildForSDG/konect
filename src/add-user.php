781cf3de56765dba76240afcb1379916258a115d
Aminu Muhammed <bulangu97@gmail.com>
2020-06-08 17:54:08 +0100

    working on the admin side

---
+++ src/add-user.php
@@ -0,0 +1,131 @@
+<?php
+defined('add-user') or 
+die('<h2>404 Not Found.<em>You are caught!</em></h2>');
+
+$error_message = '';
+$User = new model();
+$kys = '9876543210kyvxabcdefghijklmnopqrstuvCDEFGHIJKLMNOPQRSTUVWXYZjmnhoiup';
+$kys = substr(str_shuffle($kys), 0, 8); 
+if (isset($_POST['add_btn'])){  
+//'img'                =>  escape($_POST['fname']),
+$Iarray = array(
+    'first_name'         =>  escape($_POST['fname']),
+    'last_name'          =>  escape($_POST['lname']),
+    'email'              =>  escape($_POST['email']),
+    'phone'              =>  escape($_POST['phone']),
+    'password'           =>  md5($kys),
+    'gender'             =>  escape($_POST['gender']),
+    'state'              =>  escape($_POST['state']),
+    'residential_address'=>  escape($_POST['address']),
+    'role'               =>  escape($_POST['role']),
+    'is_verified'        =>  escape($_POST['verify'])
+);
+    
+        if ($Iarray['email'] == $User->authEmail("users", $Iarray['email'])) {
+            $error_message = "Error you can duplicate input";
+        }else{
+        $addd = $User->InsertData('users', $Iarray);
+        if (!$addd) {
+            $error_message = " Having some certain error, check your input ";
+        }else{
+            $error_message = " Sucessfully Added user.. ";
+        }
+        }
+}
+
+?>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-2"></div>
+		<div class="col-md-8">
+		<div class="row">
+          <div class="col-md-12">
+            <div class="card ">
+                <div class="card-header">
+			  	    <div class="card-header-right">
+					    <a href="<?php echo FARMWEB_URL; ?>user" class="btn color btn-sm">All User</a>
+				    </div>
+                    <h4 class="card-title">Add User</h4>
+                    <div class="alert color text-center">
+                        <?php echo $error_message; ?>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <form method="post" action="">
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="inputname">First Name <i style="color: red">*</i></label>
+                                <input name="fname" type="text" class="form-control" placeholder="First Name" required>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="inputname">Last Name <i style="color: red">*</i></label>
+                                <input name="lname" type="text" class="form-control" placeholder="Last Name" required>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="inputEmail4">Email <i style="color: red">*</i></label>
+                                <input name="email" type="email" class="form-control" id="inputEmail4" placeholder="Email" required>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="inputPhone">Phone</label>
+                                <input name="phone" type="tel" class="form-control" placeholder="phone number" required>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="inputGender">Gender</label><br>
+                                <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="gender" value="m">
+                                <label class="form-check-label" for="inlineRadio1">Male</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="gender" value="f">
+                                <label class="form-check-label" for="inlineRadio2">Female</label>
+                                </div>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="inputState">State <i style="color: red">*</i></label>
+                                <select name="state" class="form-control" required>
+                                    <option value="kano">Kano</option>
+                                    <option value="bauchi">Jigawa</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputAddress">Address <i style="color: red">*</i></label>
+                            <input name="address" type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="inputState">Role <i style="color: red">*</i></label>
+                                <select name="role" class="form-control" required>
+                                    <option value="kano">User</option>
+                                    <option value="bauchi">Agent</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="inputGender">Verify User</label><br>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="verify" value="1">
+                                    <label class="form-check-label" for="inlineRadio1">Yes</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="verify" value="0">
+                                    <label class="form-check-label" for="inlineRadio2">No</label>
+                                </div>
+                            </div>
+                        </div>
+                        <button name="add_btn" type="submit" class="btn btn-color">Add</button><hr>
+                    </form>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+
+		</div>
+		<div class="col-2"></div>
+	</div>
+</div>
